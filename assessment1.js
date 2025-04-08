1bbe0c47b335103423a2fb412a56d2b13ff5fb5a
laurakond <laura.kondrataite@gmail.com>
2025-04-08 12:13:22 +0100

    refactor code to follow clean code DRY concept

--- assessment1.js
+++ assessment1.js
@@ -1,22 +1,15 @@
-function f1() {
+function countDaysUntilHolidays(date, elementName) {
   var p = new Date();
-  var q = new Date("2025-12-25");
+  var q = new Date(date);
 
   var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
 
-  var variable2 = document.getElementById("my-element");
+  var variable2 = document.getElementById(elementName);
   variable2.innerText = variable;
 }
 
-function f2() {
-  var p = new Date();
-  var q = new Date("2026-01-01");
+/** Counts the days left until Christmas */
+countDaysUntilHolidays("2025-12-25", "my-element");
 
-  var variable = Math.round((q - p) / (1000 * 60 * 60 * 24));
-
-  var variable2 = document.getElementById("my-element2");
-  variable2.innerText = variable;
-}
-
-f1();
-f2();
+/** Counts the days left until New Year's Eve */
+countDaysUntilHolidays("2026-01-01", "my-element2");
